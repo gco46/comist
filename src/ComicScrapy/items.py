ae519b39c0c57a489c6160f8609168ae5fc35daf
gco46 <kojiro.tanaka0406@gmail.com>
2020-03-16 22:07:41 +0900

    add comments

--- src/ComicScrapy/items.py
+++ src/ComicScrapy/items.py
@@ -11,10 +11,10 @@ import scrapy
 class ComicImageItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    comic_key = scrapy.Field()
-    entry_url = scrapy.Field()
-    image_urls = scrapy.Field()
-    continuous_work = scrapy.Field()
-    num_images = scrapy.Field()
-    tags = scrapy.Field()
-    category = scrapy.Field()
+    comic_key = scrapy.Field()          # 固有ID(URL末尾6桁)
+    entry_url = scrapy.Field()          # 漫画エントリのURL
+    image_urls = scrapy.Field()         # 保存する漫画画像のURL
+    continuous_work = scrapy.Field()    # 連作のリンク(シンボリックリンク用)
+    num_images = scrapy.Field()         # 漫画のページ数
+    tags = scrapy.Field()               # 漫画が持つtag情報
+    category = scrapy.Field()           # カテゴリ
--- src/ComicScrapy/spiders/GetComics.py
+++ src/ComicScrapy/spiders/GetComics.py
@@ -35,18 +35,27 @@ class GetComicsSpider(scrapy.Spider):
 
     def entry_parse(self, response):
         item = ComicImageItem()
+        # category/comic_key をURLから取得してディレクトリに使用
         dir_name = response.url.split("/")[-2:]
         item['comic_key'] = "/".join(dir_name)
         item['entry_url'] = response.url
+        # css selectorで画像を指定してitemに追加
         item['image_urls'] = []
         for img_url in response.css(Css.to_images).extract():
             item['image_urls'].append(img_url)
         item['num_images'] = len(item['image_urls'])
+        # tag情報取得
+        # ページ上部と下部で二重にtag情報が取得されるため、半分だけitemに追加
         tags = response.css(Css.to_tags).extract()
         item['tags'] = tags[len(tags)//2:]
+        # category取得
         item['category'] = response.css(Css.to_category).extract_first()
+        # 連作の情報取得
+        # ページ上部と下部で二重にtag情報が取得されるため、半分だけitemに追加
         cont_list = response.css(Css.to_continuous).extract()
         cont_list = cont_list[len(cont_list) // 2:]
+        # リダイレクトしたURLを取得するために一度リクエストを送り、
+        # 返ってきたURLを連作情報としてitemに追加
         for idx in range(len(cont_list)):
             response = requests.get(cont_list[idx])
             cont_list[idx] = response.url
