80a05de48b237dc16cedf34f630bce486d37ff24
gco46 <kojiro.tanaka0406@gmail.com>
2020-04-01 23:59:09 +0900

    disabled cache (css selector does not working)

--- src/ComicScrapy/settings.py
+++ src/ComicScrapy/settings.py
@@ -89,8 +89,8 @@ IMAGES_STORE = '../data/Comics'
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 60 * 60 * 12
-HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 60 * 60 * 12
+# HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
