3c169e964a7ee797718f07d3bf1a18b179a03eb5
gco46 <kojiro.tanaka0406@gmail.com>
2021-03-07 17:42:09 +0900

    remove unnecessary pipeline

--- cc_scrapy/ComicScrapy/pipelines.py
+++ cc_scrapy/ComicScrapy/pipelines.py
@@ -60,60 +60,6 @@ class MongoPipeline(object):
         return item
 
 
-class SymboliclinkPipeline(object):
-    """
-    シンボリックリンク・ショートカット作成用パイプライン
-    """
-    # データ保存先
-    AbsDataPath = Path(myCfg.IMAGES_STORE).resolve()
-    # シンボリックリンク名(次の作品)
-    NextSym = "next"
-    # シンボリックリンク名(前の作品)
-    PreviousSym = "privious"
-
-    def process_item(self, item, spider):
-        # 連作ではない場合、処理を終了する
-        if not item['continuous_work']:
-            return item
-        cont_list = item['continuous_work']
-        entry_url = item['entry_url']
-
-        # 次の作品のみある場合
-        if cont_list.index(entry_url) == 0:
-            next_entry_url = cont_list[cont_list.index(entry_url) + 1]
-            self.make_symlink(next_entry_url, entry_url, self.NextSym)
-        # 前の作品のみある場合
-        elif cont_list.index(entry_url) + 1 == len(cont_list):
-            previous_entry_url = cont_list[cont_list.index(entry_url) - 1]
-            self.make_symlink(previous_entry_url, entry_url, self.PreviousSym)
-        # 次の作品、前の作品がある場合
-        else:
-            previous_entry_url = cont_list[cont_list.index(entry_url) - 1]
-            next_entry_url = cont_list[cont_list.index(entry_url) + 1]
-            self.make_symlink(next_entry_url, entry_url, self.NextSym)
-            self.make_symlink(previous_entry_url, entry_url, self.PreviousSym)
-        return item
-
-    def make_symlink(self, target_url, base_url, link_name):
-        # リンクターゲットのカテゴリ、idを取得
-        tgt_cat, tgt_id = target_url.split("/")[-2:]
-        base_cat, base_id = base_url.split("/")[-2:]
-        # ターゲット(sym_src)とリンクのファイル名(sym_name)を取得
-        sym_src_Path = self.AbsDataPath / tgt_cat / tgt_id
-        sym_name_Path = self.AbsDataPath / base_cat / base_id / link_name
-        if platform.system() == "Windows":
-            # windowsではショートカットを作成
-            sym_name_Path = sym_name_Path.with_name(link_name + ".lnk")
-            import win32com.client
-            shell = win32com.client.Dispatch("WScript.Shell")
-            shortcut = shell.CreateShortcut(str(sym_name_Path))
-            shortcut.TargetPath = str(sym_src_Path)
-            shortcut.save()
-        else:
-            # unix系ではシンボリックリンクを作成
-            os.symlink(str(sym_src_Path), str(sym_name_Path))
-
-
 class SaveComicPipeline(ImagesPipeline):
 
     def get_media_requests(self, item, info):
